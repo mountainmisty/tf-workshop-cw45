29026c2b6ea214dbbe79ec160ab07f7dcb3b02d0
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-11-10 11:49:51 +0100

    added version-pinning

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.38"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
