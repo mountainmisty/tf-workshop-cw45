a923da2176d99c0ed02319ff7aa8514a6423cdc1
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-11-10 13:20:57 +0100

    added count as a demo

--- ressources.tf
+++ ressources.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "app_server" {
+  count = 4
   ami           = "ami-0f61af304b14f15fb"
   instance_type = "t3.micro"
 
   tags = {
-    Name       = "Example"
+    Name       = "Server Example ${count.index+1}"
     costcenter = "42"
   }
 }
