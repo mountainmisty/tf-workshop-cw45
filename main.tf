d4884ba5cad90dd2a7536e449e648985da29af4b
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-11-10 13:08:06 +0100

    changed infra-type to t3.micro

--- main.tf
+++ main.tf
@@ -14,9 +14,10 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0f61af304b14f15fb"
-  instance_type = "t2.small"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Example"
+    costcenter = "42"
   }
 }
